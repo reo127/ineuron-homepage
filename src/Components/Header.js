fc683aafed88cac7fb910696a1d44ac12909de02
Rohan Malo <reo127.0.0.1.com>
2022-08-03 23:58:11 +0530

    header complited

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,30 +1,18 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import logo from '../images/ineuron-logo.png'
 
-// import IconButton from '@mui/material/IconButton';
-// import MenuIcon from '@mui/icons-material/Menu';
-
-
+// MUI Imports
 import { styled, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
 import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-
 
 
 
@@ -124,10 +112,10 @@ const Header = () => {
 
                 <div className="crouse_list mx-4 my-4">
 
-                    <ul className='d-flex justify-content-between align-items-center a_style'>
+                    <ul className='d-flex justify-content-between align-items-center a_style px-0' >
                         <ul>
-                            <a className="nav-link dropdown-toggle a_style" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
+                            <a className="nav-link dropdown-toggle a_style" href="https://ineuron.ai/courses" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Courses
                             </a>
                             <ul className="dropdown-menu">
                                 <li><a className="dropdown-item" href="#">Action</a></li>
@@ -136,13 +124,31 @@ const Header = () => {
                             </ul>
                         </ul>
 
-                        <li className='a_style'><a href="#"></a> One Neuron </li>
-                        <li className='a_style'><a href="#"></a> One Neuron </li>
-                        <li className='a_style'><a href="#"></a> One Neuron </li>
-                        <li className='a_style'><a href="#"></a> One Neuron </li>
-                        <li className='a_style'><a href="#"></a> One Neuron </li>
-                        <li className='a_style'><a href="#"></a> One Neuron </li>
-                        <li className='a_style'><a href="#"></a> One Neuron </li>
+                        <li >
+                            <a href="https://ineuron.ai/one-neuron" className='a_style'>One Neuron</a>
+                        </li>
+                        <li >
+                            <a href="https://jobs.ineuron.ai/" className='a_style'>Job Portal</a>
+                        </li>
+                        <li >
+                            <a href="https://jobs.ineuron.ai/" className='a_style'>Job Portal</a>
+                        </li>
+                        <li >
+                            <a href="https://internship.ineuron.ai/" className='a_style'>Internship Portal</a>
+                        </li>
+                        <li >
+                            <a href="https://affiliate.ineuron.ai/" className='a_style'>Became an affiliate</a>
+                        </li>
+                        <li >
+                            <a href="https://halloffame.ineuron.ai/" className='a_style'>Hall of frame</a>
+                        </li>
+                        <li >
+                            <a href="https://blog.ineuron.ai/" className='a_style'>Blog</a>
+                        </li>
+
+                        <li>
+                        <a href="#" className='a_style'>Company</a>
+                        </li>
                     </ul>
                 </div>
             </header>
@@ -169,20 +175,64 @@ const Header = () => {
                     open={open}
                 >
                     <DrawerHeader>
-                    <img src={logo} alt="Logo" className='img-fluid' />
+                        <img src={logo} alt="Logo" className='img-fluid w-50' />
                         <IconButton onClick={handleDrawerClose}>
                             {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
                         </IconButton>
+
+
+
                     </DrawerHeader>
                     <Divider />
                     <List>
-                        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                            <ListItem key={text} disablePadding>
-                                <ListItemButton>
-                                    <ListItemText primary={text} />
-                                </ListItemButton>
-                            </ListItem>
-                        ))}
+
+                        <ListItem disablePadding className='my-3 mx-4'>
+                            <div className="search_input_mobile d-flex align-items-center" style={{ display: 'block' }}>
+                                <i className="fa-solid fa-magnifying-glass"></i>
+                                <input type="text" className='mx-3 ' placeholder='Waht do you want to learn ?'
+                                    style={{ all: 'unset', width: '100%' }}
+                                />
+                            </div>
+                        </ListItem>
+
+                        <ListItem disablePadding className='my-3 mx-4'>
+                            <a href="https://ineuron.ai/courses" className='a_style'>Courses</a>
+                        </ListItem>
+
+
+                        <ListItem disablePadding className='my-3 mx-4'>
+                            <a href="https://ineuron.ai/one-neuron" className='a_style'>One Neuron</a>
+                        </ListItem>
+
+                        <ListItem disablePadding className='my-3 mx-4'>
+                            <a href="https://jobs.ineuron.ai/" className='a_style'>Job Portal</a>
+                        </ListItem>
+
+                        <ListItem disablePadding className='my-3 mx-4'>
+                            <a href="https://internship.ineuron.ai/" className='a_style'>Internship Portal</a>
+                        </ListItem>
+
+                        <ListItem disablePadding className='my-3 mx-4'>
+                            <a href="https://affiliate.ineuron.ai/" className='a_style'>Became an affiliate</a>
+                        </ListItem>
+
+                        <ListItem disablePadding className='my-3 mx-4'>
+                            <a href="https://halloffame.ineuron.ai/" className='a_style'>Hall of frame</a>
+                        </ListItem>
+
+                        <ListItem disablePadding className='my-3 mx-4'>
+                            <a href="https://blog.ineuron.ai/" className='a_style'>Blog</a>
+                        </ListItem>
+
+                        <ListItem disablePadding className='my-3 mx-4'>
+                            <a href="#" className='a_style'>Company</a>
+                        </ListItem>
+
+                        <ListItem disablePadding className='my-3 mx-4'>
+                            <button className="btn-nbg"> Sign In </button>
+                            <button className="btn-bg mx-4"> Sign Up </button>
+                        </ListItem>
+
                     </List>
                     <Divider />
                 </Drawer>
@@ -195,3 +245,6 @@ const Header = () => {
 }
 
 export default Header
+
+
+
