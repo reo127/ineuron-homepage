6f3eb5c2d61c7a1529e26aca41f78a0e814a3349
Rohan Malo <reo127.0.0.1.com>
2022-08-03 20:33:22 +0530

    header responsive design

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React,  { useState, useEffect }  from 'react'
 import logo from '../images/ineuron-logo.png'
 
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+
+
 const Header = () => {
+
+
+
+
+
     return (
         <>
             <header>
@@ -19,6 +29,10 @@ const Header = () => {
                         <button className="btn-bg mx-4"> Sign Up </button>
                         <button className="btn-nbg"> Sign In </button>
                     </div>
+
+                    <i class="fa-solid fa-bars hamberger"></i>
+
+
                 </div>
 
 
@@ -47,24 +61,9 @@ const Header = () => {
                         <li className='a_style'><a href="#"></a> One Neuron </li>
                         <li className='a_style'><a href="#"></a> One Neuron </li>
                     </ul>
-                    {/* 
-                    <ul>
-                        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul className="dropdown-menu">
-                            <li><a className="dropdown-item" href="#">Action</a></li>
-                            <li><a className="dropdown-item" href="#">Another action</a></li>
-                            <li><a className="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </ul> */}
-
-
                 </div>
-
-
-
             </header>
+
 
         </>
     )
